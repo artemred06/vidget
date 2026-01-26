7f77e2d2829e661c882663fcfb6bf2dbd50cc9d9
Artem Yakubov <artemya28072006@gmail.com>
2026-01-26 19:51:36 +0300

    last homework version

---
+++ src/masks.py
@@ -0,0 +1,25 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция маскировки номера банковской карты"""
+    card_number = str(card_number)
+    mask_card_number = (
+        card_number[0:4]
+        + " "
+        + card_number[4:6]
+        + "**"
+        + " "
+        + "****"
+        + " "
+        + card_number[12:]
+    )
+    return mask_card_number
+
+
+def get_mask_account(account: int) -> str:
+    """Функция маскировки номера банковского счета"""
+    account = str(account)
+    mask_account = "**" + account[-4:]
+    return mask_account
+
+
+account = input()
+print(get_mask_account(account))
