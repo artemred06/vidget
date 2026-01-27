6a836de91e34a0fe3d6bc9a00cfbf7c6bd444def
Artem Yakubov <artemya28072006@gmail.com>
2026-01-27 10:59:31 +0300

    init

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,7 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция маскировки номера банковской карты"""
     card_number = str(card_number)
-    mask_card_number = (
-        card_number[0:4]
-        + " "
-        + card_number[4:6]
-        + "**"
-        + " "
-        + "****"
-        + " "
-        + card_number[12:]
-    )
+    mask_card_number = card_number[0:4] + " " + card_number[4:6] + "**" + " " + "****" + " " + card_number[12:]
     return mask_card_number
 
 
@@ -19,4 +10,3 @@ def get_mask_account(account: int) -> str:
     account = str(account)
     mask_account = "**" + account[-4:]
     return mask_account
-
--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,19 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(information: str) -> str:
     """Функция маскировки номера карты или банковского счета"""
 
-    #Разделение строки информации полученной из аргумента
+    # Разделение строки информации полученной из аргумента
     parts = information.strip().split()
     name = " ".join(parts[:-1])
     number = parts[-1]
 
-    #При условии, что пользователь ввел мало данных, возвращаем изначальную строку
+    # При условии, что пользователь ввел мало данных, возвращаем изначальную строку
     if len(parts) < 2:
         return information
 
-    #Маскировка номера в зависимости от названия продукта
+    # Маскировка номера в зависимости от названия продукта
     if name == "Счет":
         mask_number = get_mask_account(number)
     else:
@@ -26,10 +27,10 @@ def mask_account_card(information: str) -> str:
 def get_date(input_date: str) -> str:
     """Функция определения даты из входящей строки"""
 
-    #Создание списка из дня, месяца и года
+    # Создание списка из дня, месяца и года
     list_date = input_date[:10].split(sep="-")
 
-    #Распределение по именуемым переменным
+    # Распределение по именуемым переменным
     day = list_date[2]
     month = list_date[1]
     year = list_date[0]
@@ -37,4 +38,3 @@ def get_date(input_date: str) -> str:
     date = f"{day}.{month}.{year}"
 
     return date
-
